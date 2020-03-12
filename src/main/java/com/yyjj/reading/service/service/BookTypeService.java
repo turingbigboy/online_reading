39b354827b51ab46ecaa35fa41debe2fc9cf280a
yml <yml@oudot.cn>
2020-03-12 20:42:13 +0800

    reading

---
+++ src/main/java/com/yyjj/reading/service/service/BookTypeService.java
@@ -0,0 +1,8 @@
+package com.yyjj.reading.service.service;
+
+import com.yyjj.reading.db.model.BookType;
+import com.yyjj.reading.service.base.IBaseService;
+
+public interface BookTypeService extends IBaseService<BookType> {
+
+}
---
+++ src/main/java/com/yyjj/reading/service/service/TypeService.java
@@ -0,0 +1,8 @@
+package com.yyjj.reading.service.service;
+
+import com.yyjj.reading.db.model.Type;
+import com.yyjj.reading.service.base.IBaseService;
+
+public interface TypeService extends IBaseService<Type> {
+
+}
