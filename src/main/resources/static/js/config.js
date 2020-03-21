4624061495f38c75767c5f778872b7e52c0206b2
bobo <875792791@qq.com>
2020-03-21 22:33:51 +0800

    重写springboot类，使tomcat可以运行

---
+++ src/main/resources/static/js/config.js
@@ -0,0 +1,3 @@
+axios.defaults.baseURL = 'http://localhost:8080/HTML-BOOK/';
+// axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
