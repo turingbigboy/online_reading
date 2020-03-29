8f0617276ce0f3f228624ff045f9ed650e0056bb
yml <yml@oudot.cn>
2020-03-29 12:25:05 +0800

    reading

---
+++ src/main/java/com/yyjj/reading/service/service/CommentService.java
@@ -0,0 +1,8 @@
+package com.yyjj.reading.service.service;
+
+import com.yyjj.reading.db.model.Comment;
+import com.yyjj.reading.service.base.IBaseService;
+
+public interface CommentService extends IBaseService<Comment> {
+
+}
